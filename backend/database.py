f31a289c87aa089fe176ed462a11a485f162e0e7
Mohamad Hamsho <hamsho1030@gmail.com>
2026-08-02 14:46:31 -0400

    Created the connection for the DB

---
+++ backend/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm.session import Session
+import os
+from dotenv import load_dotenv
+
+
+from sqlalchemy import create_engine  
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = os.getemv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL, 
+    echo=True,
+    connect_args={"check_same_thread": False}
+    )
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
